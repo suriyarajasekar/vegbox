d7b3cf1dc2cf0f75d7bc7f7386e08f05818bb46c
suriya <suriyarajasekar@gmail.com>
2022-06-27 21:46:32 +0530

    mat grid view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//new
+
+import {MatGridListModule} from '@angular/material/grid-list';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
+  ],
+  exports:[
+    MatGridListModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
